ed396bc080af5a89f15416099321ced9905f779f
Orioke Ayobami Simeon <ayobamiorioke4@gmail.com>
2020-01-14 21:45:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+let arr = []
+
+for(let i = 0; i < musicians.length; i++) {
+arr.push(musicians[i] + " plays "+ instruments[i])
+}
+return arr;
+}
+
+function johnLennonFacts(facts) {
+  let i = 0;
+  while(i < facts.length){
+  facts[i] += "!!!"
+  i++
+  }
+  return facts;
+}
